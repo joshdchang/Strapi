b98ae422e374f8c5c17865b9220ecf4fb7b77b8e
Josh Chang <joshchang04@gmail.com>
2022-07-22 20:02:23 -0400

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,9 +7,7 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
+          upgradeInsecureRequests: true,
         },
       },
     },
